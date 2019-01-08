7391a00d66af41bf16f99410b5ea22f15f092f6a
user <user@192.168.227.140>
2019-01-09 00:56:04 +0100

    Second commit OOP_2

---
+++ Prerequisit_OOP_2/src/Zoo.java
@@ -0,0 +1,19 @@
+
+public class Zoo {
+
+	public static void main(String[] args) {
+		Animal animal1=new Animal(3,"Mail", 23);
+		
+		animal1.eat();
+
+		Fish fish1;
+		fish1=new Fish();
+		fish1.swim();
+		
+		Bird bird1=new Bird();
+		bird1.fly();
+	
+	
+	}
+
+}
